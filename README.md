5fe90a417e60b36c2f39650e14962147c29e461f
EduardoZub <deveduardzubal@gmail.com>
2018-07-03 19:43:25 +0300

    init

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-dvvtest
+# gulpSkeleton
+### Installation
+What you need:
+1. [NPM](https://www.npmjs.com/)
+2. [Gulp v4](http://gulpjs.com/)
 
+#### Then install all dependencies
+```bash
+npm install
+```
 
-chokolat
+##### dev
+Start `gulp` {run: build, watch and browser sync}
+
+or
+
+##### prod
+Start `npm run build` {minify: images, js, css}
---
+++ src/js/main.js
@@ -0,0 +1,280 @@
+"use strict";
+
+$(document).ready(function() {
+  // slick slider script START
+  $('.selected-work-slider').slick({
+    infinite: true,
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 3,
+    speed: 1000,
+    // autoplay: true,
+    autoplaySpeed: 1000,
+    responsive: [{
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 1
+      }
+    }]
+  });
+
+  // tabs script START
+
+  $(".tab__item").not(":first").hide();
+  $(".tabs-block .tab-title").click(function() {
+    $(".tabs-block .tab-title").removeClass("active").eq($(this).index()).addClass("active");
+    $(".tab__item").hide().eq($(this).index()).fadeIn()
+  }).eq(0).addClass("active");
+});
+
+//script for chexkbox START
+$(".checkbox-btn").click(function() {
+  $(this).toggleClass("checkbox-ective");
+});
+
+//script for map START
+
+function initMap() {
+  let Mymap;
+  Mymap = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 50.0646501,
+      lng: 19.9449799
+    },
+    zoom: 4,
+    // scrollwheel:  false,
+    styles: [{
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#212121"
+        }]
+      },
+      {
+        "elementType": "labels.icon",
+        "stylers": [{
+          "visibility": "off"
+        }]
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#757575"
+        }]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [{
+          "color": "#212121"
+        }]
+      },
+      {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#757575"
+        }]
+      },
+      {
+        "featureType": "administrative.country",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#9e9e9e"
+        }]
+      },
+      {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels",
+        "stylers": [{
+          "visibility": "off"
+        }]
+      },
+      {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#bdbdbd"
+        }]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#757575"
+        }]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#181818"
+        }]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#616161"
+        }]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.stroke",
+        "stylers": [{
+          "color": "#1b1b1b"
+        }]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [{
+          "color": "#2c2c2c"
+        }]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#8a8a8a"
+        }]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#373737"
+        }]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "labels",
+        "stylers": [{
+          "visibility": "off"
+        }]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#3c3c3c"
+        }]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels",
+        "stylers": [{
+          "visibility": "off"
+        }]
+      },
+      {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#4e4e4e"
+        }]
+      },
+      {
+        "featureType": "road.local",
+        "stylers": [{
+          "visibility": "off"
+        }]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels",
+        "stylers": [{
+          "visibility": "off"
+        }]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#616161"
+        }]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#757575"
+        }]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [{
+          "color": "#000000"
+        }]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#3d3d3d"
+        }]
+      }
+    ]
+
+  });
+
+  let marker;
+  marker = new google.maps.Marker({
+    position: {
+      lat: 50.049683,
+      lng: 19.944544
+    },
+    map: Mymap,
+    icon: "img/marker.png"
+
+  });
+
+  // $.getJSON("../lib/map-style.json", function(data) {
+  //   Mymap.setOptions({styles: data});
+  // });
+
+  let InfoWindow;
+  InfoWindow = new google.maps.InfoWindow({
+    content: '<h1>Global</h1>'
+  });
+  InfoWindow.open(Mymap, marker);
+
+  marker.addListener('click', function() {
+    InfoWindow.open(Mymap, marker);
+  });
+
+
+  //script parallax START
+  var scene = document.getElementById('scene');
+  var parallaxInstance = new Parallax(scene);
+
+
+  //script pagePiling START
+  if (window.outerWidth > 1024 || window.innerWidth > 1024) {
+    $('body').removeClass('no-overflow');
+    $('.MAIN').attr('id', 'pagepiling');
+    $('#pagepiling').pagepiling({
+      normalScrollElements: '#map'
+    });
+  } else {
+    $('body').addClass('no-overflow');
+  }
+
+  window.addEventListener('resize', function() {
+    location.reload();
+  });
+
+  //scruipt for menu START
+  $('.header-burger').click(function() {
+    $('body').toggleClass('activation-menu');
+  });
+
+  $('.menu-switch').click(function() {
+    $('body').removeClass('activation-menu');
+  });
+
+};
+
+//
+//
+//
+//
